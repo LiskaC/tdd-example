c7d78ea4bfcfa05e1115da5cec7f78ef394b9708
Yelizaveta Crofts <yelizavetacrofts@Yelizavetas-MacBook-Pro.local>
2025-03-22 13:19:47 +0100

    2: Enforce desired linting rules
    Update eslint config to follow desired linting rules in project.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import stylistic from '@stylistic/eslint-plugin'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      '@stylistic': stylistic,
+      '@stylistic/ts': stylistic
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/max-len': ['error', { 'code': 80, 'ignoreStrings': true }],
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/ts/semi': ['error', 'never'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/quotes': ['error', 'single']
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,13 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest'
+import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  // in your test environment
+  dir: './'
 })
 
 const config: Config = {
@@ -19,19 +20,23 @@ const config: Config = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
+  // eslint-disable-next-line @stylistic/max-len
   // cacheDirectory: "/private/var/folders/4r/0rfdsffx0yx0460l99p0lp140000gn/T/jest_dx",
 
-  // Automatically clear mock calls, instances, contexts and results before every test
+  // Automatically clear mock calls, instances, contexts and results before
+  // every test
   clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
+  // Indicates whether the coverage information should be collected while
+  // executing the test
   collectCoverage: true,
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // An array of glob patterns indicating a set of files for which coverage
+  // information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -39,7 +44,7 @@ const config: Config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -49,7 +54,8 @@ const config: Config = {
   //   "clover"
   // ],
 
-  // An object that configures minimum threshold enforcement for coverage results
+  // An object that configures minimum threshold enforcement for coverage
+  // results
   // coverageThreshold: undefined,
 
   // A path to a custom dependency extractor
@@ -63,24 +69,31 @@ const config: Config = {
   //   "enableGlobally": false
   // },
 
-  // Force coverage collection from ignored files using an array of glob patterns
+  // Force coverage collection from ignored files using an array of glob
+  // patterns
   // forceCoverageMatch: [],
 
-  // A path to a module which exports an async function that is triggered once before all test suites
+  // A path to a module which exports an async function that is triggered once
+  // before all test suites
   // globalSetup: undefined,
 
-  // A path to a module which exports an async function that is triggered once after all test suites
+  // A path to a module which exports an async function that is triggered once
+  // after all test suites
   // globalTeardown: undefined,
 
-  // A set of global variables that need to be available in all test environments
+  // A set of global variables that need to be available in all test
+  // environments
   // globals: {},
 
-  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  // The maximum amount of workers used to run your tests. Can be specified as
+  // % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as
+  // the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
 
-  // An array of directory names to be searched recursively up from the requiring module's location
+  // An array of directory names to be searched recursively up from the
+  // requiring module's location
   moduleDirectories: [
-    "node_modules", "src"
+    'node_modules', 'src'
   ],
 
   // An array of file extensions your modules use
@@ -95,12 +108,14 @@ const config: Config = {
   //   "node"
   // ],
 
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single modul
+  // A map from regular expressions to module names or to arrays of module
+  // names that allow to stub out resources with a single modul
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
 
-  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  // An array of regexp pattern strings, matched against all module paths
+  // before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
   // Activates notifications for test results
@@ -110,7 +125,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: "ts-jest",
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -134,25 +149,29 @@ const config: Config = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
 
-  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // The paths to modules that run some code to configure or set up the testing
+  // environment before each test
   // setupFiles: [],
 
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  // A list of paths to modules that run some code to configure or set up the
+  // testing framework before each test
   // setupFilesAfterEnv: [],
 
-  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  // The number of seconds after which a test is considered as slow and reported
+  // as such in the results.
   // slowTestThreshold: 5,
 
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // A list of paths to snapshot serializer modules Jest should use for snapshot
+  // testing
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom'
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -166,7 +185,8 @@ const config: Config = {
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  // An array of regexp pattern strings that are matched against all test paths,
+  // matched tests are skipped
   // testPathIgnorePatterns: [
   //   "/node_modules/"
   // ],
@@ -183,24 +203,28 @@ const config: Config = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  // An array of regexp pattern strings that are matched against all source file
+  // paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
   //   "\\.pnp\\.[^\\/]+$"
   // ],
 
-  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+  // An array of regexp pattern strings that are matched against all modules
+  // before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
   // verbose: undefined,
 
-  // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+  // An array of regexp patterns that are matched against all source file paths
+  // before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
 
   // Whether to use watchman for file crawling
   // watchman: true,
-};
+}
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// createJestConfig is exported this way to ensure that next/jest can load the
+// Next.js config which is async
 export default createJestConfig(config)
